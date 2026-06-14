898f361309f50c8eac1c27619b8b9a43cf869502
Abhradip Paul <abhradipserampore@gmail.com>
2026-06-15 02:18:20 +0530

    add fastapi

---
+++ fastapi/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "math"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
+    {"title": "Title Six", "author": "Author Six", "category": "math"}
+]
+
+books = BOOKS
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/books/{dynamic_param}")
+async def get_book(dynamic_param):
+    return {"dynamic_param": dynamic_param}
