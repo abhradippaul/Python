ce96e82698115e674ffcd2ec9a57afa3b3191526
Abhradip Paul <abhradippaul@gmail.com>
2026-06-29 11:12:40 +0530

    add fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,22 +1,80 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-BOOKS = [
-    {"title": "Title One", "author": "Author One", "category": "science"},
-    {"title": "Title Two", "author": "Author Two", "category": "science"},
-    {"title": "Title Three", "author": "Author Three", "category": "history"},
-    {"title": "Title Four", "author": "Author Four", "category": "math"},
-    {"title": "Title Five", "author": "Author Five", "category": "math"},
-    {"title": "Title Six", "author": "Author Six", "category": "math"}
-]
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
 
-books = BOOKS
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+class BookRequest(BaseModel):
+    id: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1,max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+BOOKS = [
+    Book(1, "Title One", "Author One", "science", 4),
+    Book(2, "Title Two", "Author Two", "science", 5),
+    Book(3, "Title Three", "Author Three", "history", 3),
+    Book(4, "Title Four", "Author Four", "math", 4),
+    Book(5, "Title Five", "Author Five", "history", 5),
+    Book(6, "Title Six", "Author Two", "science", 2),
+]
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+        
+@app.get("/books/")
+async def read_book(book_category: str, book_author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == book_author.casefold() and book.get("category").casefold() == book_category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
-@app.get("/books/{dynamic_param}")
-async def get_book(dynamic_param):
-    return {"dynamic_param": dynamic_param}
+@app.post("/books/create_book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+    return new_book
+
+@app.put("/books/update_book")
+async def update_book(new_book=Body()):
+    for book,i in BOOKS:
+        if new_book.casefold() == book.get("title").casefold():
+            BOOKS[i] = new_book
+            return new_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title):
+    for i in range(len(BOOKS)):
+        if book_title.casefold() == BOOKS[i].get("title").casefold():
+            removed_book = BOOKS.pop(i)
+            return removed_book
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+        
+@app.get("/books/{book_title}/")
+async def read_book(book_title: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold() and book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
